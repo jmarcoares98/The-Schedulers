939e84ad950dec108be2a2ca381e8cb4d4f58f89
Daniel Hazelton <dlhazelton1@aol.com>
2020-11-01 12:55:31 -0800

    Eclipse2019 code

---
+++ projectScheduler-old/Member.java
@@ -0,0 +1,43 @@
+package projectScheduler;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Member {
+
+	String name;
+	String email;
+	
+	public Member(String Iname, String Iemail) {
+		setName(Iname);
+		setEmail(Iemail);
+	}
+	
+	public String getName() 
+    { 
+        return name; 
+    } 
+	public String getEmail() 
+    { 
+        return email; 
+    } 
+	
+	public void setName(String Iname) 
+    { 
+        this.name = Iname; 
+    } 
+	
+	public void setEmail(String Iemail) 
+    { 
+        this.email = Iemail; 
+    } 
+	
+	public String formatMember(int i) 
+    { 
+        String out = "";
+		out = "("+i+")"+" "+name+": "+email;
+		return out; 
+    } 
+	
+	
+}
--- projectScheduler/TestMember.java
+++ projectScheduler/TestMember.java
@@ -2,44 +2,26 @@ package projectScheduler;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-
 import static org.mockito.Mockito.*;
 
 
-public class TestMember {
+
+public class Testmember {
 
 	
 	Member spyMember = spy(new Member("bob","123@hotmail"));
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
+	
+	
 	@Test
 	public void testformatMember() {
-		doReturn("Daniel").when(spyMember).getName();
-		doReturn("DLH@gmail.com").when(spyMember).getEmail();
-		
-		assertEquals("(5) Daniel: DLH@gmail.com",spyMember.formatMember(5));
-		
-		fail("Not yet implemented");
+		//doReturn("Daniel").when(spyMember).getName();
+		//doReturn("DLH@gmail.com").when(spyMember).getEmail();
+		//Member M = new Member("Daniel","DLH@gmail.com");
+		//assertEquals(5,5);
+		assertEquals("(5) bob: 123@hotmail",spyMember.formatMember(5));
+		//assertEquals("(5) Daniel: DLH@gmail.com",M.formatMember(5));
+		//fail("Not yet implemented");
 	}
 
 }
